613236817c9003172e2cc8d52f77367981dcb6e7
Dries Vints <dries.vints@gmail.com>
2013-08-04 15:02:48 +0200

    Add unit tests for helpers file
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

---
+++ tests/HelpersTest.php
@@ -0,0 +1,32 @@
+<?php
+
+class HelpersTest extends PHPUnit_Framework_TestCase {
+
+	public function testIsFalse()
+	{
+		$this->assertTrue(is_false(false));
+	}
+
+	public function testIsNotFalse()
+	{
+		$this->assertFalse(is_false(null));
+	}
+
+	public function testIsTrue()
+	{
+		$this->assertTrue(is_true(true));
+	}
+
+	public function testIsNotTrue()
+	{
+		$this->assertFalse(is_true(null));
+	}
+
+	public function testLastKey()
+	{
+		$data = array('foo' => 'bar', 'baz' => 'foz');
+
+		$this->assertEquals('baz', last_key($data));
+	}
+
+}
